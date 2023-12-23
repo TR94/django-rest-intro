d48bd00ed2f5000e749d96a2773483892e86729f
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2023-12-23 09:32:11 +0000

    Set up project and profile model

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+# django secret key 
+os.environ['SECRET_KEY']= ""
+
+# cloudinary connection
+os.environ['CLOUDINARY_URL']= 'cloudinary://659183824575138:-cnW2HCg5Ocn2ETg6w2XHmX1kdA@dtgt7gfx7'
+
