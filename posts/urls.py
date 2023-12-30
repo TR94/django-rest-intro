c55d4a6fdbf88736e7cd001736aca98105788890
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2023-12-30 16:11:04 +0000

    Added postlist view tests

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,5 @@ from posts import views
 urlpatterns = [
     path('posts/', views.PostList.as_view()), 
     # we call as_view because its a class based view - see views.py
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
