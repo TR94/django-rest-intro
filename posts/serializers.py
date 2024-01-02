18b16d1ba860b3b6494218e9f59ccec6892e97fe
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-02 18:59:02 +0000

    Added filtring and new fields to profile

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers 
 from .models import Post
+from likes.models import Likes
 
 class PostSerializer(serializers.ModelSerializer):
     # this can be treated like a model form, add extra fields as required
@@ -7,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image =  serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     # built in rest framework validation uses "validate_[field name]"
     def validate_image(self, value):
@@ -29,12 +31,21 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            liked = Likes.objects.filter(
+                owner=user, post=objpyt
+            ).first()
+            return liked.id if liked else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title', 'content', 'image',
-            'image_filter'
+            'image_filter', 'like_id'
         ]
 
         # note that id has to be added specifically to these fields. 
