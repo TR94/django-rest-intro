bbbc43b0764e35730f022d9cbfe61eed19e86cbe
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-02 12:07:58 +0000

    Created followers app, serializer, models, views

---
+++ followers/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+# List takes care of GET method and Create does the POST method
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all() # this line can be used to filter data if required 
+
+    def perform_create(self, serializer):
+        # this associates a user with the follow
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    # List takes care of GET, DELETE methods as used in POST views.py
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowSerializer
+    queryset = Follower.objects.all()
