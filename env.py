934ef5daf1891308ac453de9f7f9f8be33d2baa8
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-03 12:14:25 +0000

    Added Json web token authentication

--- env.py
+++ env.py
@@ -6,3 +6,6 @@ os.environ['SECRET_KEY']= ""
 # cloudinary connection
 os.environ['CLOUDINARY_URL']= 'cloudinary://659183824575138:-cnW2HCg5Ocn2ETg6w2XHmX1kdA@dtgt7gfx7'
 
+# during development session authentication is used. This will be changed to JSON Web Tokens for production release
+os.environ['DEV'] = '1'
+
