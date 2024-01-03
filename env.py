c6d433c34637213b01b2c0638ac39de58c939078
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-03 19:25:42 +0000

    Preparing for deployment

--- env.py
+++ env.py
@@ -1,11 +1,13 @@
 import os
 
 # django secret key 
-os.environ['SECRET_KEY']= ""
+os.environ.setdefault('SECRET_KEY', "django-nowsecure-5qsmz-$rg!o*fm+3l^=616az_2h4%*=db_81#!huc(%lsiip8&")
 
 # cloudinary connection
 os.environ['CLOUDINARY_URL']= 'cloudinary://659183824575138:-cnW2HCg5Ocn2ETg6w2XHmX1kdA@dtgt7gfx7'
 
 # during development session authentication is used. This will be changed to JSON Web Tokens for production release
 os.environ['DEV'] = '1'
+
+os.environ['DATABASE_URL'] = "postgres://gtrcuszs:5bIWrF50ags5GzDJ1Tt2qlDvaowch0Ym@tyke.db.elephantsql.com/gtrcuszs"
 
